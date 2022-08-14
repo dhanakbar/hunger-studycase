40af46afd9ac21d95907efed89b8cfaead6bef51
dhanakbar <maulana261100@gmail.com>
2022-08-14 07:05:59 +0700

    Update code

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-import style from './layout.module.css'
 function Layout({children}) {
   return (
     <div className={style.layout}>
