dd5d4d9cae2698f51a5bfa0296e88a9af391cf72
dhanakbar <maulana261100@gmail.com>
2022-08-14 07:07:19 +0700

    Delete layout style

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 function Layout({children}) {
   return (
-    <div className={style.layout}>
+    <div>
       <Navbar/>
       <div>
         {children}
